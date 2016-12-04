c10f5184e71b2e47574479ae55f988004f105fe8
Valdeci Gomes <valdeci@coderade.in>
2016-12-04 19:32:33 -0200

    using StyleUrls to use style in files on component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'mw-app',
   templateUrl: 'app/app.component.html',
-  styles: [`
-    h1{ color: #FFFFFF }
-    .description {
-      font-style : italic;
-      color: green;
-    }
-  `]
+  styleUrls: ['app/app.component.css']
+   
 })
 export class AppComponent {}

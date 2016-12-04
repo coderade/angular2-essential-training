54638cfa16ea2a8768dd201d653ece014c93f88f
Valdeci Gomes <valdeci@coderade.in>
2016-12-04 19:24:18 -0200

    using file templates

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  template: 
-    `<h1>My App</h1
-    <p>Keeping track of the media I want to watch.</p>`
+  templateUrl: 'app/app.component.html'
+    
 })
 export class AppComponent {}
